a0e96175c051ab4b66b6154ae767a9866486d0e7
Omkar12218549 <121383250+Omkar12218549@users.noreply.github.com>
2022-12-28 13:53:24 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+a=int(input("enter the first no.:- "))
+b=int(input("enter the second no.:- "))
+opr=input("enter the operator......(+,/,*,-,%)")
+if opr=="+":
+    print(a+b)
+elif opr=="+":
+    print(a+b)
+elif opr=="-":
+    print(a-b)
+elif opr=="*":
+    print(a*b)
+elif opr=="%":
+    print(a%b)
+else:
+    print("Invalid number")
